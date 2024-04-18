6a837d97ac05f615eb409eaa76c097d78aadbbaf
Khalil <khalil.j.khalil2@gmail.com>
2024-04-18 15:09:39 -0400

    Added const Menu to be able to use hrefs in multiple areas. Added navbar-menu and menu_container class. added toggleMenu onclicks to menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
 import './navbar.css';
+
+const Menu = () => (
+  <>
+  <p><a href='#home'>Home</a></p>
+  <p><a href='#wgpt3'>What Is GPT</a></p>
+  <p><a href='#possibility'>Open AI</a></p>
+  <p><a href='#features'>Case Studies</a></p>
+  <p><a href='#blog'>Library</a></p>
+  </>
+)
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -13,11 +23,7 @@ const Navbar = () => {
           <img src={logo} alt='logo'/>
         </div>
         <div className='gpt3__navbar-links_container'>
-          <p><a href='#home'>Home</a></p>
-          <p><a href='#wgpt3'>What Is GPT</a></p>
-          <p><a href='#possibility'>Open AI</a></p>
-          <p><a href='#features'>Case Studies</a></p>
-          <p><a href='#blog'>Library</a></p>
+          <Menu/>
         </div>
       </div>
       <div className='gpt3__navbar-sign'>
@@ -25,6 +31,17 @@ const Navbar = () => {
         <button type='button'>Sign Up</button>
       </div>
       <div className='gpt3__navbar-menu'>
+        {toggleMenu
+          ? <RiCloseLine color='#fff'size={27} onClick={() => setToggleMenu(false)}/>
+          : <RiMenu3Line color='#fff'size={27} onClick={() => setToggleMenu(true)}/>
+        }
+        {toggleMenu && (
+          <div className='gpt3__navbar-menu_container scale-up-center'>
+            <div className='gpt3__navbar-menu_container-links'>
+              <Menu/>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
