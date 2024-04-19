67157569407635a86feb4026ac0e9b78e68afa30
Khalil <khalil.j.khalil2@gmail.com>
2024-04-19 15:14:59 -0400

    format change in Brand.jsx. Changed div to im g in brand.css

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -4,12 +4,22 @@ import './brand.css';
 
 const Brand = () => {
   return (
-    <div className='.gpt3__brand section__padding'>
-      <div><img src={google} alt='google'/></div>
-      <div><img src={slack} alt='slack'/></div>
-      <div><img src={atlassian} alt='atlassian'/></div>
-      <div><img src={dropbox} alt='dropbox'/></div>
-      <div><img src={shopify} alt='shopify'/></div>
+    <div className='gpt3__brand section__padding'>
+      <div>
+        <img src={google} alt='google'/>
+      </div>
+      <div>
+        <img src={slack} alt='slack'/>
+      </div>
+      <div>
+        <img src={atlassian} alt='atlassian'/>
+      </div>
+      <div>
+        <img src={dropbox} alt='dropbox'/>
+      </div>
+      <div>
+        <img src={shopify} alt='shopify'/>
+      </div>
     </div>
   )
 }
