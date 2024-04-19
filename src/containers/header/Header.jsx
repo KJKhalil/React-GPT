0243b6ec1f4a2d87e47b7869961e872465a4a4d2
Khalil <khalil.j.khalil2@gmail.com>
2024-04-19 14:07:20 -0400

    Finished header.css

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,29 +6,29 @@ import ai from '../../assets/ai.png'
 const Header = () => {
   return (
     <div className='gpt3__header section__padding' id='home'>
-        <div className='gpt3__header-content'>
+      <div className='gpt3__header-content'>
           
-          {/*Header Title And The Text Under It*/}
-          <h1 className='gradient__text'>Let’s Build Something Amazing With GPT-3 OpenAI</h1>
-          <p>Yet bed any for traveling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        {/*Header Title And The Text Under It*/}
+        <h1 className='gradient__text'>Let’s Build Something Amazing With GPT-3 OpenAI</h1>
+        <p>Yet bed any for traveling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
           
-          {/*Input Box For The Users Email And The Button To Start The Process*/}
-          <div className='gpt3__header-content__input'>
-            <input type='email' placeholder='Your Email Address'/>
-            <button type='button'>Get Started</button>
-          </div>
-
-          {/*The Image Showing Recent Visitors And The Text Under It*/}
-          <div className='gpt3__header-content_people'>
-            <img src={people} alt='people'/>
-            <p>1,600 people requested access a visit in last 24 hours.</p>
-          </div>
-
-          {/*The Image In The Top Right Of The Page*/}
-          <div className='gpt3__header-image'>
-            <img src={ai} alt='ai'/>
-          </div>
+        {/*Input Box For The Users Email And The Button To Start The Process*/}
+        <div className='gpt3__header-content__input'>
+          <input type='email' placeholder='Your Email Address'/>
+          <button type='button'>Get Started</button>
         </div>
+
+        {/*The Image Showing Recent Visitors And The Text Under It*/}
+        <div className='gpt3__header-content_people'>
+          <img src={people} alt='people'/>
+          <p>1,600 people requested access a visit in last 24 hours.</p>
+        </div>
+      </div>
+
+      {/*The Image In The Top Right Of The Page*/}
+      <div className='gpt3__header-image'>
+        <img src={ai} alt='ai'/>
+      </div>
     </div>
   )
 }
