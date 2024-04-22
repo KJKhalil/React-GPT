9a735a13a722e591625d51b2e7286741850aced8
Khalil <khalil.j.khalil2@gmail.com>
2024-04-22 14:47:35 -0400

    fixed typo from whpt3 to wgpt3

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -5,7 +5,7 @@ import './whatGPT3.css';
 
 const WhatGPT3 = () => {
   return (
-    <div className='gpt3__whatgpt3 section__margin' id='whpt3'>
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
       
       {/*Feature title, text functionality in Feature.jsx*/}
       <div className='gpt3__whatgpt3-feature'>
