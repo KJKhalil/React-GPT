cc916376a9ee78c80766f33e504db9ca1584e859
Khalil <khalil.j.khalil2@gmail.com>
2024-04-22 12:38:02 -0400

    Got feature.css from inspiration figma project. Fixed More Typos Affecting Functionality.

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -5,10 +5,10 @@ import './whatGPT3.css';
 
 const WhatGPT3 = () => {
   return (
-    <div className='gpt3__whatgpt3 section_margin' id='whpt3'>
+    <div className='gpt3__whatgpt3 section__margin' id='whpt3'>
       
       {/*Feature title, text functionality in Feature.jsx*/}
-      <div className='gpt3__whatgpt3=feature'>
+      <div className='gpt3__whatgpt3-feature'>
         <Feature title='What Is GPT-3' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.'/>
       </div>
       
