d0c6ffc90c4ee60844c5710d5d64213403049acf
Khalil <khalil.j.khalil2@gmail.com>
2024-04-23 13:33:50 -0400

    Started On Blog.jsx, Imported Articles.jsx From components, Created Header Section And Blog Content Sections

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Article } from '../../components';
 import './blog.css';
 
 const Blog = () => {
   return (
-    <div>
-        Blog
+    <div className='gpt3__blog section__padding' id='blog'>
+      
+      {/*Header Content At The Top Of This Section*/}
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>A Lot Is Happening, We Are Blogging About It.</h1>
+      </div>
+
+      {/*First Blog To The Left*/}
+      <div className='gpt__blog-container'>
+        <div className='gpt3__blog-container__groupA'>
+          <Article/>
+        </div>
+
+        {/*Four Other Blogs To The Right*/}
+        <div className='gpt3__blog-container__groupB'>
+          <Article/>
+          <Article/>
+          <Article/>
+          <Article/>
+        </div>
+      </div>
     </div>
   )
 }
