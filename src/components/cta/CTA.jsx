3e34d5ebfbc4bcfeebbdccb3612d8e35ccdb1804
Khalil <khalil.j.khalil2@gmail.com>
2024-04-23 12:56:07 -0400

    Started and Finished CTA.jsx. Created A Content And Button Div

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,10 +1,18 @@
+/*This Page Is For The Section Right Under The 'Open Ai' Section*/
 import React from 'react';
 import './cta.css';
 
 const CTA = () => {
   return (
-    <div>
-        CTA
+    <div className='gpt3__cta'>
+      <div className='gpt3__cta-content'>
+        <p>Request Early Access To Get Started</p>
+        <h3>Register Today & Start Exploring The Endless Possibilities.</h3>
+      </div>
+
+      <div className='gpt3__cta-btn'>
+        <button type='button'>Get Started</button>
+      </div>
     </div>
   )
 }
