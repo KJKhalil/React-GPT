10b82707058e6c8eb9c49cbfaacaa64ab998d6da
Khalil <khalil.j.khalil2@gmail.com>
2024-04-23 15:50:55 -0400

    Started Footer.jsx, imported logo.svg from assets, created heading, button, and links divs

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import logo from '../../assets/logo.svg';
 import './footer.css';
 
 const Footer = () => {
   return (
-    <div>
-        Footer
+    <div className='gpt3__footer section__padding'>
+      <div className='gpt3__footer-heading'>
+        <h1 className='gradient__text'>Do You Want To Step In To The Future Before Others</h1>
+      </div>
+
+      <div className='gpt3__footer-btn'>
+        <>Request Early Access</>
+      </div>
+
+      <div className='gpt3__footer-links'>
+        <div className='gpt3__footer-links__logo'>
+          <img src={logo} alt='logo'/>
+          <p>5134 RandomStreet Rd, RandomState 11111</p>
+        </div>
+      </div>
     </div>
   )
 }
