ac1e714703b4b111bf1bb61cd72429384ab192eb
Khalil <khalil.j.khalil2@gmail.com>
2024-04-23 16:23:55 -0400

    Finished Footer.jsx, Finished the Link Divs, Created copyright div

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -18,6 +18,34 @@ const Footer = () => {
           <img src={logo} alt='logo'/>
           <p>5134 RandomStreet Rd, RandomState 11111</p>
         </div>
+
+        {/*First 3 Links On The Left Href Can Be Added*/}
+        <div className='gpt3__footer-links__div'>
+          <h4>Links</h4>
+          <p>CompanyLinkedin</p>
+          <p>CompanyFacebook</p>
+          <p>CompanyTwitter</p>
+        </div>
+
+        {/*Second 3 Links In The Middle Href Can Be Added*/}
+        <div className='gpt3__footer-links__div'>
+          <h4>Company</h4>
+          <p>Careers</p>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+        </div>
+
+        {/*Last 3 Links On The Right Href Can Be Added*/}
+        <div className='gpt3__footer-links__div'>
+          <h4>Contact Us</h4>
+          <p>555-555-5555</p>
+          <p>CompanyCustomerServiceEmail</p>
+          <p>5134 RandomStreet Rd, RandomState 11111</p>
+        </div>
+
+        <div className='gpt3__footer-copyright'>
+          <p>© 2024 GPT-3. All Rights Reserved.</p>
+        </div>
       </div>
     </div>
   )
